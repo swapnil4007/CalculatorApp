c3996bea93599606f32bd63203239d0b0186f061
student3 <student3@PC3>
2026-03-05 11:22:45 +0530

    Added Substraction Feature Dt. 05.03.2026

--- src/com/msedcl/main/CalculatorMain.java
+++ src/com/msedcl/main/CalculatorMain.java
@@ -7,6 +7,7 @@ public class CalculatorMain {
 	public static void main(String[] args) {
 		CalculatorUtil calculatorUtil = new CalculatorUtil();
 		System.out.println("Addition ="+ calculatorUtil.addition(5, 7));
+		System.out.println("Substraction ="+ calculatorUtil.substraction(20, 5));
 	}
 
 }
