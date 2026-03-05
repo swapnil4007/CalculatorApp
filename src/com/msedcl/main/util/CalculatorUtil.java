0043e6d48640425984c94a63226ddb308f2e4bdb
Administrator <Administrator@host.docker.internal>
2026-03-05 12:16:33 +0530

    checking conflicts

--- src/com/msedcl/main/util/CalculatorUtil.java
+++ src/com/msedcl/main/util/CalculatorUtil.java
@@ -2,14 +2,26 @@ package com.msedcl.main.util;
 
 public class CalculatorUtil {
 	public int addition(int n1, int n2) {
-		
+		System.out.println(" Addition ");
 		return n1+n2;
 		
 	}
 	
 	public int substraction(int n1, int n2) {
-		
+		System.out.println(" substraction ");
 		return n1-n2;
 	}
 
+	
+	public int multiplication(int n1, int n2) {
+		System.out.println(" multiplication ");
+
+		
+		return n1*n2;
+	}
+	
+	
+	
+
+	
 }
